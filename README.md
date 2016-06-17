32f841778c9c1da5be7143ad8f1034734cfe4220
Cédric <cedced19@gmail.com>
2016-06-17 10:57:35 +0200

    Add unit tests

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Generate a png from an office document or from a pdf file.
 
 `imagemagick` and `unoconv` are __required__ to use this module.
+
+[![Build Status](https://travis-ci.org/cedced19/machinepack-document-preview.svg?branch=master)](https://travis-ci.org/cedced19/machinepack-document-preview)
---
+++ test/generate.js
@@ -0,0 +1,55 @@
+var chai = require('chai');
+var join = require('path').join;
+chai.use(require('chai-fs'));
+
+describe('DocumentPreview Generator', function () {
+  var generate = require('../').generate;
+
+  it('should convert a word document to a png file', function (done) {
+    generate({
+      from: join(__dirname, '/files/cv-template.docx'),
+      to: join(__dirname, '/files/cv-template.png'),
+      mime: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    }).exec({
+
+      error: function (err) {
+        done(err);
+      },
+
+      noConvertion: function () {
+        done('This file should be converted');
+      },
+
+      success: function () {
+        chai.expect(join(__dirname, '/files/cv-template.png')).to.be.a.file('should have created a preview');
+        done();
+      }
+
+    });
+
+  });
+
+  it('should not convert a jpeg', function (done) {
+    generate({
+      from: join(__dirname, '/files/image.jpg'),
+      to: join(__dirname, '/files/image.png'),
+      mime: 'image/jpeg'
+    }).exec({
+
+      error: function (err) {
+        done(err);
+      },
+
+      noConvertion: function () {
+        done();
+      },
+
+      success: function () {
+        done('This file should not be converted');
+      }
+
+    });
+
+  });
+
+});
